be12703bf2a77996f14d61d3d1360f88db001b87
Guilherme Santos Coelho <32603185+GuilhermeSantosCoelho@users.noreply.github.com>
2021-09-29 20:45:29 -0300

    Add files via upload

--- Cliente.py
+++ Cliente.py
@@ -1,10 +1,61 @@
 from socket import *
+import json
 serverName = 'localhost'
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
-sentence = input('Input lowercase sentence:')
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print ('From Server:', modifiedSentence.decode())
-clientSocket.close()
+
+option = -1
+
+class User:
+    def __init__(self):
+        self.username = ''
+        
+    def setUsername(self, username):
+        self.username = username
+    
+user = User()
+
+def createUser():
+    user.username = input("Digite um nome de usuário: ")
+    if len(user.username) == 0:
+        print("Nome de usuário inválido")
+        
+def createRoom():
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName,serverPort))
+    if len(user.username) == 0:
+        print("Nome de usuário inválido")
+    else:
+        clientSocket.send(json.dumps({"action": "createRoom", "username": user.username}).encode())
+        returnData = clientSocket.recv(1024)
+        print(returnData.decode())
+    clientSocket.close()
+    
+def findRoom():
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName,serverPort))
+    if len(user.username) == 0:
+        print("Nome de usuário inválido")
+    else:
+        clientSocket.send(json.dumps({"action": "findRoom"}).encode())
+        returnData = clientSocket.recv(1024)
+        print(json.loads(returnData.decode()))
+    clientSocket.close()
+        
+while option != '0':
+    print(" MENU")
+    print("1 - Criar usuário")
+    print("2 - Criar sala")
+    print("3 - Encontrar sala")
+    option = input("Digite uma opção: ")
+    
+    if option == '1':
+        createUser()
+    elif option == '2':
+        createRoom()
+    elif option == '3':
+        findRoom()
+ 
+# clientSocket.send(sentence.encode())
+# modifiedSentence = clientSocket.recv(1024)
+# print ('From Server:', modifiedSentence.decode())
+# clientSocket.close()
--- Servidor.py
+++ Servidor.py
@@ -1,28 +1,50 @@
 from socket import *
+import json
+
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 print('O servidor está rodando')
-# while True:
-# 	connectionSocket, addr = serverSocket.accept()
-# 	sentence = connectionSocket.recv(1024).decode()
-# 	capitalizedSentence = sentence.upper()
-# 	connectionSocket.send(capitalizedSentence.encode())
-# 	connectionSocket.close()
 
+rooms = []
+colors = ['heart', 'diamonds', 'spades', 'clubs']
 
 class Card:
     def __init__(self, value, color):
         self.value = value
         self.color = color
 
-colors = ['heart', 'diamonds', 'spades', 'clubs']
+class Room:
+	def __init__(self, id, userHost):
+		self.id = id
+		self.userHost = userHost
+		self.deck = [Card(value, color) for value in range(1, 14) for color in colors]
+  
+	def getHost(self):
+		return self.userHost
 
+	def resetRoom(self):
+		self.deck = [Card(value, color) for value in range(1, 14) for color in colors]
 
-def InitializeRound():
-    deck = [Card(value, color) for value in range(1, 14) for color in colors]
-    print(len(deck))
+def createRoom(user):
+	userHasRoom = [room for room in rooms if room.getHost() == user]
+	if len(userHasRoom) == 1:
+		return False
+	rooms.append(Room(len(rooms) + 1, user))
+	return len(rooms) + 1
 
+while True:
+	connectionSocket, addr = serverSocket.accept()
+	data = connectionSocket.recv(1024).decode()
+	data = json.loads(data)
 
-InitializeRound()
+	if(data['action'] == 'createRoom'):
+		if createRoom(data['username']):
+			connectionSocket.send(("Sala criada: " + str(len(rooms))).encode())
+		else:
+			connectionSocket.send('Não foi possível criar a sala'.encode())
+	if(data['action'] == 'findRoom'):
+		connectionSocket.send(json.dumps(rooms).encode())
+  
+	connectionSocket.close()
