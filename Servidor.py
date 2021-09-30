cec50ecb5ce52247a18b9d43f7046759b25f9b9c
GuilhermeSantosCoelho <guilhermesantoscoelho2000@gmail.com>
2021-09-29 22:54:41 -0300

    Implementando arquivo Entity

--- Servidor.py
+++ Servidor.py
@@ -1,4 +1,5 @@
 from socket import *
+from Entity import *
 import json
 
 serverPort = 12000
@@ -7,32 +8,33 @@ serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 print('O servidor está rodando')
 
-rooms = []
-colors = ['heart', 'diamonds', 'spades', 'clubs']
+rooms = []	
+users = []
+userId = 1
 
-class Card:
-    def __init__(self, value, color):
-        self.value = value
-        self.color = color
+def createUser(user):
+	userExists = [user for user in users if user.getUsername() == user]
+	print(list(users))
+	if len(userExists) > 0:
+		return {"success": False}
+	newUser = User(user, userId)
+	users.append(newUser)
+	return {"success": True, "id": newUser.id}
 
-class Room:
-	def __init__(self, id, userHost):
-		self.id = id
-		self.userHost = userHost
-		self.deck = [Card(value, color) for value in range(1, 14) for color in colors]
-  
-	def getHost(self):
-		return self.userHost
-
-	def resetRoom(self):
-		self.deck = [Card(value, color) for value in range(1, 14) for color in colors]
-
-def createRoom(user):
+def createRoom(user, userId):
 	userHasRoom = [room for room in rooms if room.getHost() == user]
-	if len(userHasRoom) == 1:
+	if len(userHasRoom) > 0:
 		return False
-	rooms.append(Room(len(rooms) + 1, user))
+	newRoom = Room(len(rooms) + 1, user)
+	newRoom.lobby.append(userId)
+	rooms.append(newRoom)
 	return len(rooms) + 1
+
+def findRoom():
+    returnRooms = []
+    for room in rooms:
+        returnRooms.append(room.getRoomInfo())
+    return returnRooms
 
 while True:
 	connectionSocket, addr = serverSocket.accept()
@@ -40,11 +42,16 @@ while True:
 	data = json.loads(data)
 
 	if(data['action'] == 'createRoom'):
-		if createRoom(data['username']):
+		if createRoom(data['username'], data['userId']):
 			connectionSocket.send(("Sala criada: " + str(len(rooms))).encode())
 		else:
 			connectionSocket.send('Não foi possível criar a sala'.encode())
-	if(data['action'] == 'findRoom'):
-		connectionSocket.send(json.dumps(rooms).encode())
+   
+	elif(data['action'] == 'findRoom'):
+		connectionSocket.send(json.dumps(findRoom()).encode())
+  
+	elif(data['action'] == 'newUser'):
+		userId += 1
+		connectionSocket.send(json.dumps(createUser(data['username'])).encode())
   
 	connectionSocket.close()
